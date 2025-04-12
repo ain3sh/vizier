47807d1a1697f7e37ce59b312423a5bbbcfef2a4
smnuran <smnuran@gmail.com>
2025-04-12 01:35:12 -0400

    user profile update calls added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import auth
+from routers import auth, user 
 from database import register_lifespan_events
 
 app = FastAPI()
 register_lifespan_events(app)
 app.include_router(auth.router)
+app.include_router(user.router)
 
